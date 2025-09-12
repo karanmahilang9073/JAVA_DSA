a0064a7c5132b21373b1f002531fb35feb6d7d22
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2025-09-12 20:45:04 +0530

    Initial commit

---
+++ pattern/Hollow_pyramid.java
@@ -0,0 +1,23 @@
+package pattern;
+
+public class Hollow_pyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // stars & spaces
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1 || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            // move to next line
+            System.out.println();
+        }
+    }
+}
