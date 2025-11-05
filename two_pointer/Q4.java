199907e4207cd521acb39d77295fe56efd6a7160
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2025-11-05 18:09:39 +0530

    dsa

---
+++ two_pointer/Q4.java
@@ -0,0 +1,44 @@
+package two_pointer;
+// Pairs
+// You’re given an array of n unique integers and a target value k.
+// Find how many pairs of array elements have a difference equal to k
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q4 {
+    public static int pairs(int[] arr, int k){
+        Arrays.sort(arr);
+        int left = 0;
+        int right = 1;
+        int n = arr.length;
+        int count = 0;
+        while (right < n) {
+            int diff = arr[right] - arr[left];
+            if (diff == k ) {
+                count++;
+                left++;
+                right++;
+            }else if (diff < k) {
+                right++;
+            }else{
+                left++;
+            }
+            if (left == right) {
+                right++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(pairs(arr, k));
+    }
+}
---
+++ two_pointer/Q5.java
@@ -0,0 +1,38 @@
+package two_pointer;
+//Picking Numbers
+// Given an array of integers, find the length of the longest subsequence where the absolute difference between any two elements ≤ 1.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q5 {
+    public static int pickingnum(int[] arr){
+        Arrays.sort(arr);
+        int left = 0;
+        int maxlen = 0;
+
+        int n = arr.length;
+        for (int right = 0; right < n; right++) {
+            while (arr[right] - arr[left] > 1) {
+                left++;
+            }
+            maxlen = Math.max(maxlen, right - left +1);
+        }
+        return maxlen;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(pickingnum(arr));
+    }
+}
+
+//O(n log n) — sorting + O(n) window
+
+// 6
+// 4 6 5 3 3 1
+// 3
