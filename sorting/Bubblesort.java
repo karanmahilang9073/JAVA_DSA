4aed8760911a928dbda45941ac52a6ead0aa95d5
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2025-11-07 22:10:14 +0530

    .

--- sorting/Bubblesort.java
+++ sorting/Bubblesort.java
@@ -7,13 +7,10 @@ public class Bubblesort {
          Bubble(arr);
          System.out.println(Arrays.toString(arr));
     }
-
     static void Bubble(int[] arr){
         //run the steps n-1 times
         for (int i = 0; i < arr.length; i++) {
-            //for each step, max item will come at the last respective index
             for (int j = 1; j < arr.length - i; j++) {
-                //swap if the element is smaller than previous item
                 if(arr[j] < arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
